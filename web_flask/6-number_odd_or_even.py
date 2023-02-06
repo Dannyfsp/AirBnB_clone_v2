6195cfce25863b78293390d5a76c3fa698bfaaec
Dannyfsp <femoscopilala@gmail.com>
2023-02-06 07:27:14 +0000

    serving the number_odd_or_even page with flask

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+'''
+A simple Flask web application
+'''
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+'''The Flask application instance'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''The home page'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_page(text):
+    '''The c page'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+@app.route('/python', defaults={'text': 'is cool'})
+def python_page(text):
+    '''The python page'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number_page(n):
+    '''The number page'''
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n=None):
+    '''The number_template page'''
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<n>')
+def number_odd_or_even(n=None):
+    '''The number_odd_or_even page'''
+    if isinstance(n, int):
+        if n % 2:
+            e_o = 'odd'
+        else:
+            e_o = 'even'
+        return render_template('6-number_odd_or_even.html', n=n, e_o=e_o)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
